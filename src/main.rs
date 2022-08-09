cf3a1912c9923a58f85499aae6122ae8f32fece5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-09 18:30:06 -0500

    :tada: Begin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use dotenvy::dotenv_iter;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let items = match dotenv_iter() {
+        Ok(items) => items,
+        Err(error) => {
+            eprintln!("Problem opening the .env: '{:?}'.", error.to_string());
+            process::exit(1);
+        }
+    };
+
+    for item in items {
+        let (key, val) = item.unwrap();
+
+        println!("{}={}", key, val);
+    }
 }
